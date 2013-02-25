984350e441e0988d4653489ba45b68cbb5bdc566
Cameron Edwards <noremac2021@gmail.com>
2013-02-25 16:37:22 -0500

    updated client, fixed packet responses

--- CookOutAppServer/src/com/wayne/cookoutapp/server/net/Connection.java
+++ CookOutAppServer/src/com/wayne/cookoutapp/server/net/Connection.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 import java.net.Socket;
 import java.util.Arrays;
 
-import javax.xml.bind.DatatypeConverter;
 
 import org.apache.log4j.Logger;
 
--- CookOutAppServer/src/com/wayne/cookoutapp/server/net/packet/client/RequestComboInfoPacket.java
+++ CookOutAppServer/src/com/wayne/cookoutapp/server/net/packet/client/RequestComboInfoPacket.java
@@ -1,6 +1,8 @@
 package com.wayne.cookoutapp.server.net.packet.client;
 
 import java.sql.SQLException;
+import java.util.Map;
+
 import com.wayne.cookoutapp.server.CookOutAppServer;
 import com.wayne.cookoutapp.server.net.packet.server.BadPacket;
 import com.wayne.cookoutapp.server.net.packet.server.ComboInfoPacket;
@@ -16,8 +18,16 @@ public class RequestComboInfoPacket extends ClientPacket {
 			return new BadPacket();
 		
 		try {
+			byte flavor1 = data[1];
+			byte flavor2 = data[2];
 			
-			return new ComboInfoPacket(CookOutAppServer.getDatabase().getComboRating(data[1], data[2]));
+			Map<Integer, String> flavors = CookOutAppServer.getDatabase()
+					.getFlavors();
+
+			if (flavors.get(flavor1) == null || flavors.get(flavor2) == null)
+				return new ErrorMessagePacket("Flavors don't exist.");
+			
+			return new ComboInfoPacket(CookOutAppServer.getDatabase().getComboRating(flavor1, flavor2));
 		} catch (SQLException e) {
 			return new ErrorMessagePacket("Server SQL error.");
 		}
